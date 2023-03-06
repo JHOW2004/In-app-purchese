8bf0f5b896cbc8be72195c979fe89510dfe3f30c
Jonathas Pereira Gonçalves <91168264+JHOW2004@users.noreply.github.com>
2023-03-06 15:54:38 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,42 @@
+import { Platform, StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+
+    Title: {
+        fontSize: 30,
+        marginBottom: 40,
+        marginTop: 40,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+
+
+    button: {
+        width: 275,
+        height: 100,
+        justifyContent: 'center',
+        borderRadius: 200,
+        marginTop: 20,
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+
+    textButton: {
+        color: "#f8f8ff",
+        textAlign: 'center',
+        fontSize: 20,
+    },
+
+    divImg: { 
+        marginTop: 70,
+        justifyContent: "center",
+        alignContent: 'center',
+        alignItems: 'center'
+    },
+
+    img: {
+        width: 351,
+        height: 164.7,
+    },
+});
+
+export default styles
